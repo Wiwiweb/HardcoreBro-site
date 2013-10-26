cf13bb44b56227cd4bcd6a853babe22f12a3923d
Wiwiweb <wiwiweb2@gmail.com>
2013-10-25 17:13:32 -0700

    Add main page ratio button

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var $mainContainer;
 var $player;
 var $chat;
+var $ratioChangeButton;
 
 var isRatio169 = true;
 var playerWidthPercent = .69;
@@ -11,6 +12,10 @@ var PLAYER_BAR_SIZE = 30;
 $(document).ready(function () {
     $mainContainer = $('#main-container');
     $player = $('#player');
+    $chat = $('#chat');
+    $ratioChangeButton = $('#ratio-change');
+
+    $ratioChangeButton.click(switchRatio);
 
     if(Math.random() <= 0.05) {
         $('#normal-banner').hide();
@@ -20,6 +25,22 @@ $(document).ready(function () {
     resizePlayer();
     var debouncedPlayerResize = _.debounce(resizePlayer, 50);
     $(window).resize(debouncedPlayerResize);
+});
+
+$(window).load(function() {
+    player.$channelDropdown.change(function() {
+        console.debug("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        switch (player.$channelDropdown.val()) {
+            case 'twitch1':
+            case 'twitch2':
+            case 'twitch-custom':
+                set169Mode(true);
+                break;
+            case 'livestream':
+                set169Mode(false);
+                break;
+        }
+    });
 });
 
 function resizePlayer() {
@@ -42,13 +63,15 @@ function set169Mode(ratio169) {
         playerWidthPercent = .69;
         $player.width("69%");
         $chat.width("29%");
+        $ratioChangeButton.text("Change to 4:3 mode");
     } else {
         ratio = 4 / 3;
         playerWidthPercent = .59;
         $player.width("59%");
         $chat.width("39%");
+        $ratioChangeButton.text("Change to 16:9 mode");
     }
-    $player.twitchSet169Mode(ratio169);
+    player.twitchSet169Mode(ratio169);
     isRatio169 = ratio169;
     resizePlayer();
 }
